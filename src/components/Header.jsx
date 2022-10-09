e29408dafd066496634038865b4f0a933a0b6812
Jenna <jenna48071@gmail.com>
2022-10-09 17:09:39 -0500

    fixed dropdown bug

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,22 +3,25 @@ import '../CSS/dropdown.css'
 import { useState } from 'react';
 
 function Header() {
-
 const [checked, setChecked] = useState(false);
+const [locationChecked, setLocationChecked] = useState(false);
 const styles = {
-dropdownNav:{
-top: checked ? '10px' : null,
-transitionDuration: '.35s',
-
-},
-dropdownBtn:{
-top: checked ? '42px' : null,
-transitionDuration: '.35s',
-}
+  dropdownNav:{
+    top: checked ? '10px' : null,
+    transitionDuration: '.35s',
+  },
+  dropdownBtn:{
+    top: checked ? '42px' : null,
+    transitionDuration: '.35s',
+  }
 }
 
 const handleCheckbox = () => {
   setChecked(!checked)
+}
+// LOCATION DROPDOWN BTN
+const handleButtonClick = () => {
+setLocationChecked(!locationChecked)
 }
 
 return (
@@ -38,40 +41,45 @@ return (
         <img width='150px' src={logo} alt='logo' className='logo'></img>
       </a>
 
-      <hr className='logo-hr hiddenMobile'/>
+      <hr className='logo-hr hiddenMobile' />
 
       <div className="mobile-rightSide">
         {/* LOCATIONS DROPDOWN */}
-          <label className='location-button' htmlFor="locationBtn"><i class="fa-solid fa-location-dot locationLogo"></i></label>
-          <input type="checkbox" id='locationBtn' />
+        <button onClick={handleButtonClick} id='locationBtn'></button>
+        <div onMouseOver={()=> setLocationChecked(true)} onMouseLeave={() => setLocationChecked(false)}
+          className="locations-hover-container">
+          <label className='location-button' htmlFor="locationBtn"><i
+              class="fa-solid fa-location-dot locationLogo"></i></label>
           <div className=" hiddenMobile location-label-container">
             <h3 className='locations-label'>LOCATIONS</h3>
-            <i class=" down-arrow fa-solid fa-sort-down"></i>
+            <i onClick={handleButtonClick} class="down-arrow fa-solid fa-sort-down"></i>
           </div>
-        
 
-          <ul className='slide'>
+          <ul id={locationChecked ? 'location-pressed' : 'location-notPressed' } className='slide'>
             <div className="slide-container">
               <p className='search-location'>Search</p>
               <div className='input-container'>
                 <input placeholder={'city & state or zip'} type="text" id="location-input" />
                 <button className='goBtn'>GO</button>
               </div>
-              <h4 className='current-location'><span><i class="fa-solid fa-location-arrow location-arrow"></i></span> USE CURRENT LOCATION</h4>
+              <h4 className='current-location'><span><i class="fa-solid fa-location-arrow location-arrow"></i></span>
+                USE
+                CURRENT LOCATION</h4>
             </div>
           </ul>
 
-          <hr className='logo-hr hr2 hiddenMobile'/>
+          <hr className='logo-hr hr2 hiddenMobile' />
+        </div>
 
         {/* HAMBURGER MENU */}
         <div className="hamburger-menu">
-          <input type="checkbox" id='menu__toggle' />
+          <input checked={checked} type="checkbox" id='menu__toggle' />
           <label style={styles.dropdownBtn} onClick={handleCheckbox} class="menu__btn" htmlFor="menu__toggle">
             <span></span>
           </label>
 
           <ul className='menu__box'>
-            <li><a className='menu menu__item' href='#'>MENU <i class="fa-solid fa-greater-than more-menu"></i></a></li>
+            <li><a className='menu menu__item' href='#'>MENU <i class="fa-solid fa-greater-than more-menu"></i></a></li> {/* Dropdown */}
             <li><a className='menu__item' href='#'>CATERING</a></li>
             <li><a className='menu__item' href='#'>GIFT CARDS</a></li>
             <li><a className='menu__item' href='#'>CAREERS</a></li>
@@ -83,18 +91,13 @@ return (
             <li><a className='menu__item' href='#'>CONTACT</a></li>
           </ul>
         </div>
-
+        {/* HAMBURGER MENU END */}
       </div>
 
-
-      {/* HIDDEN IN MOBILE */}
-      <div className="locations-dropdown hiddenMobile">
-
-      </div>
-
+      {/* NAV HIDDEN IN MOBILE */}
       <nav className='main-navList-container hiddenMobile'>
         <ul className='navList'>
-          <li>MENU</li> {/* Dropdown */}
+          <li>MENU</li>
           <li>CATERING</li>
           <li>GIFT CARDS</li>
           <li>CAREERS</li>
