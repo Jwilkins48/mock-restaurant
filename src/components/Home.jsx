fd5888036fdf9ae83e8a82d4cb895165370e0414
Jenna <jenna48071@gmail.com>
2022-10-15 21:42:42 -0500

    adding main content

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,16 +9,24 @@ function Home() {
         <Header />
 
         <div className="home-display-container">
+
           <div className="inner-display">
-            <div className="display-container">
-              <figure className='product-img'>
-                <img width='580px' src={taco} alt="taco" />
+              <figure className='product-figure'>
+                <img className='product-img' src={taco} alt="taco" />
                 <figcaption className='product-caption'>
                   <span>tuesdays<br /> <small>were <br /> made for <br /></small>tacos</span>
                 </figcaption>
               </figure>
+
+              <div className="product-content">
+                <h6>TUESDAYS JUST GOT BETTER.</h6>
+                <p>Your choice of three beef or chicken tacos, crispy or soft, plus rice and beans
+                  for only $5.49 every Tuesday, starting at 10:30 AM! Spice it up the way you like with Rosa’s FREE Salsa Bar.
+                </p>
+                <span className='product-price'>$5.49</span>
+                <a href='#'>VIEW MENU <i class='ico-arrows'></i></a>
+              </div>
             </div>
-          </div>
         </div>
       
     </section>
