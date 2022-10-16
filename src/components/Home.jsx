a00ec9db01b8af0748da89c31c427351300a2a73
Jenna <jenna48071@gmail.com>
2022-10-15 19:22:27 -0500

    began placing home image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import Header from './Header'
+import taco from '../imgs/product-large.png'
+import '../CSS/home.css'
 
 function Home() {
   return (
     <section id='Home'>
         <Header />
+
+        <div className="home-display-container">
+          <div className="inner-display">
+            <div className="display-container">
+              <figure className='product-img'>
+                <img width='580px' src={taco} alt="taco" />
+                <figcaption className='product-caption'>
+                  <span>tuesdays<br /> <small>were <br /> made for <br /></small>tacos</span>
+                </figcaption>
+              </figure>
+            </div>
+          </div>
+        </div>
       
     </section>
   )
