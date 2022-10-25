75f290accf87fac0fcb54efa6811b9456905287d
Jenna <jenna48071@gmail.com>
2022-10-24 22:07:22 -0500

    continue van section

--- src/components/CaterSection.jsx
+++ src/components/CaterSection.jsx
@@ -5,17 +5,14 @@ function CaterSection() {
 return (
 <section className='weCater-section'>
     <div className="cater-container">
+        <div className="cater-content-section">
+            <h1>WE CATER!</h1>
+            <h5>LET US CATER YOUR NEXT EVENT</h5>
+            <button>LEARN MORE</button>
+        </div>
 
-        <div className="cater-inner-section">
-            <div className="cater-content-section">
-                <h1>WE CATER!</h1>
-                <h5>LET US CATER YOUR NEXT EVENT</h5>
-                <button>LEARN MORE</button>
-            </div>
-
-            <div className="van-cater-section">
-                <img className='van-img' src={van} alt='van'></img>
-            </div>
+        <div className="van-cater-section">
+            <img className='van-img' src={van} alt='van'></img>
         </div>
 
     </div>
