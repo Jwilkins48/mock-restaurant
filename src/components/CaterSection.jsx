bd7a6f734059a1dc0c6dc4c5aa549d61d0a81b91
Jenna <jenna48071@gmail.com>
2022-10-26 17:28:29 -0500

    finished gallery section

--- src/components/CaterSection.jsx
+++ src/components/CaterSection.jsx
@@ -14,7 +14,29 @@ return (
         <div className="van-cater-section">
             <img className='van-img' src={van} alt='van'></img>
         </div>
+    </div>
 
+    <div className="gallery-section">
+        <div className="gallery-content">
+            <div className="all-gallery-items">
+                <div className="gallery-item"><figure className="gallery-item-img"><img alt='gallery img 1' src={require('../imgs/gallery-section/gallery-1.jpg')}></img></figure></div>
+                <div className="gallery-item"><figure className="gallery-item-img"><img alt='gallery img 2' src={require('../imgs/gallery-section/gallery-2.jpg')}></img></figure></div>
+                <div className="gallery-item"><figure className="gallery-item-img"><img alt='gallery img 3' src={require('../imgs/gallery-section/gallery-3.jpg')}></img></figure></div>
+                <div className="gallery-item"><figure className="gallery-item-img"><img alt='gallery img 4' src={require('../imgs/gallery-section/gallery-4.jpg')}></img></figure></div>
+                <div className="gallery-item"><figure className="gallery-item-img"><img alt='gallery img 5' src={require('../imgs/gallery-section/gallery-5.jpg')}></img></figure></div>
+                <div className="gallery-item"><figure className="gallery-item-img"><img alt='gallery img 6' src={require('../imgs/gallery-section/gallery-6.jpg')}></img></figure></div>
+                <div className="gallery-item"><figure className="gallery-item-img"><img alt='gallery img 7' src={require('../imgs/gallery-section/gallery-7.jpg')}></img></figure></div>    
+                <div className="gallery-item"><figure className="gallery-item-img"><img alt='gallery img 8' src={require('../imgs/gallery-section/gallery-8.jpg')}></img></figure></div>     
+                <div className="gallery-item"><figure className="gallery-item-img"><img alt='gallery img 9' src={require('../imgs/gallery-section/gallery-9.jpg')}></img></figure></div>               
+                <div className="gallery-item"><figure className="gallery-item-img"><img alt='gallery img 10' src={require('../imgs/gallery-section/gallery-10.jpg')}></img></figure></div>                
+                <div className="gallery-item"><figure className="gallery-item-img"><img alt='gallery img 11' src={require('../imgs/gallery-section/gallery-11.jpg')}></img></figure></div>                
+                <div className="gallery-item"><figure className="gallery-item-img"><img alt='gallery img 12' src={require('../imgs/gallery-section/gallery-12.jpg')}></img></figure></div>
+            </div>
+        </div>
+
+        <div className="gallery-actions">
+            <button className='gallery-btn'>VIEW OUR MENU</button>
+        </div>
     </div>
 
 </section>
