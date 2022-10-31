769d1cf2199af8b39c2f87465963b0e093a24a45
Jenna <jenna48071@gmail.com>
2022-10-31 18:38:44 -0500

    added banner

--- src/components/Promotional.jsx
+++ src/components/Promotional.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import '../CSS/promotional.css'
 import promoteImg from '../imgs/slide-image-4.jpg'
+import bannerImg from '../imgs/life-is-better.png'
+
 
 function promotional() {
   return (
-    <div>
+    <section>
         <div className="promote-container">
             <figure className='promote-img'><img alt='black beans' src={promoteImg}></img></figure>
             <div className="promote-content-container">
@@ -15,7 +17,15 @@ function promotional() {
                 </div>
             </div>
         </div>
-    </div>
+
+        <div className="banner">
+            <div className="banner-container">
+                <div className="banner-img">
+                    <img src={bannerImg} alt="rosa's banner" />
+                </div>
+            </div>
+        </div>
+    </section>
   )
 }
 
