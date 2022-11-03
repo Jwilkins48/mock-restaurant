cb267e3d45573977b1fe8e96f7d4516d84e95c6d
Jenna <jenna48071@gmail.com>
2022-11-03 15:07:07 -0500

    added footer

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,18 +21,18 @@ return (
             <span className='caption'>tuesdays<br /> <small>were <br /> made for <br /></small>tacos</span>
           </figcaption>
         </figure>
-        </div>
+      </div>
 
-        <div className="product-content">
-          <div className="content-container">
-            <h6 className='content-header'>TUESDAYS JUST GOT BETTER.</h6>
-            <p className='home-message'>Your choice of three beef or chicken tacos, crispy or soft, plus rice and beans
-              for only $5.49 every Tuesday, starting at 10:30 AM! Spice it up the way you like with Rosa’s FREE Salsa
-              Bar.
-            </p>
-            <span className='product-price'>$5.49</span>
-            <a className='view-menu-btn' href='/'>VIEW MENU <i class='ico-arrows menu-arrow'></i></a>
-          </div>
+      <div className="product-content">
+        <div className="content-container">
+          <h6 className='content-header'>TUESDAYS JUST GOT BETTER.</h6>
+          <p className='home-message'>Your choice of three beef or chicken tacos, crispy or soft, plus rice and beans
+            for only $5.49 every Tuesday, starting at 10:30 AM! Spice it up the way you like with Rosa’s FREE Salsa
+            Bar.
+          </p>
+          <span className='product-price'>$5.49</span>
+          <a className='view-menu-btn' href='/'>VIEW MENU <i class='ico-arrows menu-arrow'></i></a>
+        </div>
       </div>
     </div>
   </div>
@@ -40,6 +40,44 @@ return (
   <CaterSection />
   <Social />
   <Promotional />
+
+  <footer className='footer'>
+    <div className="footer-container">
+      <div className="footer-inner">
+        <div className="footer-left">
+          <nav>
+            <ul>
+              <li>PRESS</li>
+              <li>CONTACT</li>
+              <li>SUBSCRIBE</li>
+            </ul>
+          </nav>
+
+          <div className="footer-socials">
+            <ul>
+              <li><i className='footer-facebook'></i></li>
+              <li><i className='footer-instagram'></i></li>
+              <li><i className='footer-twitter'></i></li>
+            </ul>
+          </div>
+
+          <ul className="footer-links">
+            <li className='small-footerLink'>SITE DESIGNED BY ALGO+RHYTHM</li>
+            <li className='footerLink'>PRIVACY POLICY</li>
+            <li className='footerLink'>TERMS OF SERVICE</li>
+          </ul>
+
+        </div>
+        <div className="footer-right">
+          <a href className='back-to-top-container'>
+            <i className='to-top-arrow'></i>
+            <span>BACK TO TOP</span>
+          </a>
+          <p>AUTHENTIC IS OUR SPECIAL INGREDIENT</p>
+        </div>
+      </div>
+    </div>
+  </footer>
 
 </section>
 )
